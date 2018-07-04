9e64f9152c188d665df7400671144b65c447dbc8
jramirez <jramirez@kdu.cl>
2018-07-04 17:26:27 -0400

    Desafio cumplo

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -6,3 +6,26 @@ function isHttpSuccess(code) {
 };
 
 var app = angular.module("app",['ui.router','chart.js']);
+
+app.service('inicioService', function (service) {
+    return {
+        callApiUF: function (filter) {
+
+            var url = "/desafio/uf"
+            return service.rest({
+                method: 'post',
+                url: url,
+                data: filter
+            });
+        },
+        callApiDolar: function (filter) {
+
+            var url = "/desafio/dolar"
+            return service.rest({
+                method: 'post',
+                url: url,
+                data: filter
+            });
+        }
+    };
+});
