b315d64cdbf8dff4b4c6b69f554d115366a73f34
jramirez <jramirez@kdu.cl>
2018-07-04 16:55:44 -0400

    Desafio cumplo

--- app/assets/javascripts/app/controller/inicio.js
+++ app/assets/javascripts/app/controller/inicio.js
@@ -71,16 +71,6 @@ app.controller('inicioController', function ($scope, inicioService) {
         };
         $scope.graficoDolar = true;
     }
-    function getDates(startDate, stopDate) {
-        var dateArray = [];
-        var currentDate = moment(startDate);
-        var stopDate = moment(stopDate);
-        while (currentDate <= stopDate) {
-            dateArray.push( moment(currentDate).format('DD/MM/YYYY') )
-            currentDate = moment(currentDate).add(1, 'days');
-        }
-        return dateArray;
-    };
     $scope.callApi = function(){
         if($scope.busqueda.fechaHasta == null || $scope.busqueda.fechaHasta == ""){
             alert("Ingrese fecha desde");
@@ -98,61 +88,41 @@ app.controller('inicioController', function ($scope, inicioService) {
         request.fechaDesde = new Date($scope.busqueda.fechaDesde.setDate($scope.busqueda.fechaDesde.getDate()-1));
         request.fechaHasta = $scope.busqueda.fechaHasta;
         inicioService.callApiUF(request).then(function (response){
-            $scope.data.uf.valores = response.data.UFs.filter(function(item) {
-                var d2 = Date.parse(item.Fecha);
-                if(d2 <= $scope.busqueda.fechaHasta)
-                    return true;
-                else
-                    return false;
-            });
-            console.log($scope.data.uf.valores.length);
-            $scope.data.uf.promedio = ($scope.data.uf.valores.map(function(obj) {return parseFloat(obj.Valor.replace(".","").replace(",","."))})
-                .reduce(function(a, b) { return a + b }) / $scope.data.uf.valores.length).toFixed(2).toLocaleString();
-            $scope.data.uf.maximo = $scope.data.uf.valores.reduce(function (valorAnterior, valorActual){
-                if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) > parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
-                    return valorAnterior;
-                } else {
-                    return valorActual;
-                }
-            }).Valor;
-            $scope.data.uf.minimo = $scope.data.uf.valores.reduce(function (valorAnterior, valorActual){
-                if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) < parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
-                    return valorAnterior;
-                } else {
-                    return valorActual;
-                }
-            }).Valor;
+            parseData($scope.data.uf, response.data.UFs);
             $scope.initGraphUF();
 
             inicioService.callApiDolar(request).then(function (response){
-
-                $scope.data.dolar.valores = response.data.Dolares.filter(function(item) {
-                    var d2 = Date.parse(item.Fecha);
-                    if(d2 <= $scope.busqueda.fechaHasta)
-                        return true;
-                    else
-                        return false;
-                });
-                $scope.data.dolar.promedio = ($scope.data.dolar.valores.map(function(obj) {return parseFloat(obj.Valor.replace(".","").replace(",","."))})
-                    .reduce(function(a, b) { return a + b }) / $scope.data.dolar.valores.length).toFixed(2).toLocaleString();
-                $scope.data.dolar.maximo = $scope.data.dolar.valores.reduce(function (valorAnterior, valorActual){
-
-                    if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) > parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
-                        return valorAnterior;
-                    } else {
-                        return valorActual;
-                    }
-                }).Valor;
-                $scope.data.dolar.minimo = $scope.data.dolar.valores.reduce(function (valorAnterior, valorActual){
-                    if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) < parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
-                        return valorAnterior;
-                    } else {
-                        return valorActual;
-                    }
-                }).Valor;
+                parseData($scope.data.dolar, response.data.Dolares);
                 $scope.initGraphDolar();
             });
         });
 
     };
+
+    function parseData(data, response){
+        data.valores = response.filter(function(item) {
+            var d2 = Date.parse(item.Fecha);
+            if(d2 <= $scope.busqueda.fechaHasta)
+                return true;
+            else
+                return false;
+        });
+        data.promedio = (data.valores.map(function(obj) {return parseFloat(obj.Valor.replace(".","").replace(",","."))})
+            .reduce(function(a, b) { return a + b }) / data.valores.length).toFixed(2).toLocaleString();
+        data.maximo = data.valores.reduce(function (valorAnterior, valorActual){
+
+            if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) > parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
+                return valorAnterior;
+            } else {
+                return valorActual;
+            }
+        }).Valor.replace(".","").replace(",",".");
+        data.minimo = data.valores.reduce(function (valorAnterior, valorActual){
+            if(parseFloat(valorAnterior.Valor.replace(".","").replace(",",".")) < parseFloat(valorActual.Valor.replace(".","").replace(",","."))){
+                return valorAnterior;
+            } else {
+                return valorActual;
+            }
+        }).Valor.replace(".","").replace(",",".");
+    }
 });
